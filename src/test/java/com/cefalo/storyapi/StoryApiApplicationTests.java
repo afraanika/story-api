e7f8f19a5cd9ba7aae5a6a60dc3ee97d7d13aeb2
unknown <afraanikaa@gmail.com>
2022-08-29 10:33:06 +0600

    initial project setup

---
+++ src/test/java/com/cefalo/storyapi/StoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefalo.storyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
