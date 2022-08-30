5d8e89910ca3c3827114ecfe5de6741837b7af05
unknown <afraanikaa@gmail.com>
2022-08-30 12:48:18 +0600

    Issue 2 : implement user all endpoints

---
+++ src/main/java/com/cefalo/storyapi/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.cefalo.storyapi.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cefalo.storyapi.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	public boolean existsByEmail(String email);
+
+	public Optional<User> findByEmail(String email);
+	
+}
