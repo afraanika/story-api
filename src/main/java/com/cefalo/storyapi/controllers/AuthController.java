b42cdeda9120fe018721cab80da38c55ff3d42d8
unknown <afraanikaa@gmail.com>
2022-08-31 10:21:59 +0600

    Issue 3 implement signup signin

---
+++ src/main/java/com/cefalo/storyapi/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package com.cefalo.storyapi.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cefalo.storyapi.models.User;
+import com.cefalo.storyapi.services.AuthService;
+
+@RestController
+public class AuthController {
+	
+	@Autowired
+	private AuthService authService;
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/signup")
+	public ResponseEntity<Optional<User>> addUser (@RequestBody User user) {
+		try {
+			return new ResponseEntity<>(authService.addUser(user), HttpStatus.CREATED);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	} 
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/signin")
+	public ResponseEntity<Optional<User>> checkUser(@RequestBody User user) {
+		Optional<User> userOptional = authService.checkUser(user);
+		if(userOptional.isPresent()) return ResponseEntity.ok(userOptional);
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+
+}
