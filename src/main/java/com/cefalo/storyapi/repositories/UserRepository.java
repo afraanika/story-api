04dacef1b270b1a671e1f2507d5f0e73962d54da
unknown <afraanikaa@gmail.com>
2022-09-08 13:13:37 +0600

    Issue 6: implement story endpoints without jwt

--- src/main/java/com/cefalo/storyapi/repositories/UserRepository.java
+++ src/main/java/com/cefalo/storyapi/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.cefalo.storyapi.repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cefalo.storyapi.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
 
 	public Optional<User> findByEmail(String email);
