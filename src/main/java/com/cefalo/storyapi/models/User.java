eefd269c38b1f8a854bc5f00ff6f75ad949b2dd0
unknown <afraanikaa@gmail.com>
2022-10-07 11:51:00 +0600

    Issue 0: updated user

--- src/main/java/com/cefalo/storyapi/models/User.java
+++ src/main/java/com/cefalo/storyapi/models/User.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Email;
 
-@SuppressWarnings("serial")
 @Entity
 @Table(name = "users")
 public class User implements UserDetails{
